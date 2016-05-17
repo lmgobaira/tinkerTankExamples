c5cbd77381941390b458af739fd0030db45b54bc
Lance Gobaira <lance.gobaira@skyscanner.com>
2016-05-17 01:44:04 -0400

    Adding Blink Example and GPIO.cleanup()

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ MAY 2016
 """
 
 # Import necessary external libraries
+import time
+
 import RPi.GPIO as GPIO
 import requests
 from flask import Flask
@@ -22,6 +24,7 @@ GPIO.setup(led_pin, GPIO.OUT)
 
 
 # Defining flask routes
+# The @symbol denotes a decorator. Decorators make complex things easier.
 @app.route("/on/")
 def led_on():
     """
@@ -38,8 +41,27 @@ def led_off():
     Turn on LED pin 4 to ON any request to http://<server>/off
     :return:
     """
-    GPIO.output(led_pin, 1)
+    GPIO.output(led_pin, 0)
     return "Light is Off"
+
+
+@app.route("/blink/")
+def led_blink():
+    """
+    Blink little light blink!
+    :return:
+    """
+
+    # Blink 10 times with 1 second pause between state changes
+    # ***Caution**** : Long running process should be executed ASYNC (Queue, thread, Celery, etc)
+    count = 0
+    while count < 10:
+        GPIO.output(led_pin, 0)
+        time.sleep(1)
+        GPIO.output(led_pin, 1)
+        time.sleep(1)
+        count += 1
+    return "It was blinking. Whoa!"
 
 
 @app.route("/ipinfo/")
@@ -48,7 +70,7 @@ def ip_info():
     Makes a GET request using requests library to http://jsonip.com/ then dumps response
     :return:
     """
-    # Make a request to external API
+    # Make a request to external API (also try http://www.timeapi.org/utc/now, or any other  REST API endpoint)
     r = requests.get('http://jsonip.com/')
     return r.text
 
@@ -62,15 +84,22 @@ def hello():
     return "<h1> Tinker Tank V: Python</h1><p>Hola Amigos</p>"
 
 
-# Some simple error Catching
+# Some simple 404 error Catching
 @app.errorhandler(404)
 def page_not_found(error):
-    return "Doh.. {}".format(error)
+    """
+    Prints error message on screen using .format method and string substitution.
+
+    :param error:
+    :return:
+    """
+    return "<h1>Doh..</h1> {}".format(error)
 
 
-# This block of code executes when the program is called directly (i.e. not via interpreter)
+# This block of code executes when the program is called directly (not via interpreter)
 if __name__ == "__main__":
     # Turns on debug mode
     app.debug = True
     # Starts app and binds to all interfaces ( ie makes app available on the network)
     app.run(host="0.0.0.0")
+    GPIO.cleanup()
